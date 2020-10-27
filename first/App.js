015a970fcaef39e7bed53c8cf856fe720f42b19e
WADAGAMER <32307478+WADAGAMER@users.noreply.github.com>
2020-10-27 13:41:32 -0700

    cambios

--- first/App.js
+++ first/App.js
@@ -23,7 +23,7 @@ import {
   DebugInstructions,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
-const hello=()=><Text>Hello world</Text>;
+const hello=()=>(<Text>Hello world</Text>);
 
 const App: () => React$Node = () => {
   return (
